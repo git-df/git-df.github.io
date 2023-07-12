111c3b32ddd40cea84edec6f059a023e16aa974d
Dawid Florian <dawidflorian99@gmail.com>
2023-07-12 23:33:13 +0200

    language mod

--- js/langs.js
+++ js/langs.js
@@ -1,29 +1,37 @@
 let langlinks = document.querySelectorAll(".langs a")
 
 let data = {
-    english: {
-        menu: {
-            about: "About",
-            resume: "Resume",
-            contact: "Contact",
-            cv: "CV"
-        }
-    },
-    polish: {
-        menu: {
-            about: "O mnie",
-            resume: "Życiorys",
-            contact: "Kontakt",
-            cv: "CV"
-        }
-    }
+    english: [
+        ["about", "About"],
+        ["resume","Resume"],
+        ["contact","Contact"],
+        ["cv","CV"],
+        ["heroh1","Hi, I'm Dawid Florian"],
+    ],
+    polish: [
+        ["about", "O\u00A0mnie"],
+        ["resume","Życiorys"],
+        ["contact","Kontakt"],
+        ["cv","CV"],
+        ["heroh1","Cześć, jestem Dawid Florian"],
+    ]
 }
 
-langlinks.forEach(el => {
-    el.addEventListener("click", () =>{
-        document.querySelectorAll(".langs .active").forEach(x => {
-            x.classList.remove("active");
+langlinks.forEach(link => {
+    link.addEventListener("click", () =>{
+            document.querySelectorAll(".langs .active").forEach(activelink => {
+                activelink.classList.remove("active");
         })
-        el.classList.add("active");
+        link.classList.add("active");
+
+
+        let selectlang = link.getAttribute("Language");
+        let dictlang = selectlang == "1" ? data.polish : data.english;
+
+        dictlang.forEach(dict => {
+            document.getElementsByName(dict[0]).forEach(el => {
+                el.textContent = dict[1];
+            });
+        })
     })
 })
