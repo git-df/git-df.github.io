9321526b1e508d81e4dbe02c5eb76173f56cf02b
Dawid Florian <dawidflorian99@gmail.com>
2023-08-28 21:47:43 +0200

    projscts fix

--- js/langs.js
+++ js/langs.js
@@ -58,7 +58,7 @@ let langsDict = [
         "W trakcie studiów zrealizowałem wiele projektów z wykorzystaniem .NET, w tym system wynajmu samochodów (Blazor, Rest API, MS SQL). Ta podróż wiele mnie nauczyła, szczególnie w praktycznym zastosowaniu tych technologii."
     ],
     ["1h5_projects","This is a comprehensive car rental system developed as a part of an engineering degree capstone project.", "Jest to kompleksowy system wynajmu samochodów opracowany w ramach projektu dyplomowego na zakończenie studiów inżynierskich."],
-    ["2h5_projects","This project is focused on developing a feature-rich and customizable blogging platform. With Bloggie, users can create, manage, comment and like blog posts with a user-friendly interface.", "Projekt ten koncentruje się na opracowaniu bogatej w funkcje i konfigurowalnej platformy blogowej. Dzięki aplikacji możliwe jest tworzenie, zarządzanie, komentowanie oraz lajkowania postów na blogu za pomocą przyjaznego dla użytkownika interfejsu."],
+    ["2h5_projects","With Bloggie, users can create, manage, comment and like blog posts with a user-friendly interface.", "Dzięki aplikacji możliwe jest tworzenie, zarządzanie, komentowanie oraz lajkowania postów na blogu za pomocą przyjaznego dla użytkownika interfejsu."],
     ["3h5_projects","This project aims to provide a comprehensive solution for managing various aspects of a car detailing business.", "Projekt ten ma na celu dostarczenie kompleksowego rozwiązania do zarządzania różnymi aspektami działalności Car Detailingu."],
     
     ["h2_contact","Contact", "Kontakt"],
