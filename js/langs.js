a6a9d1b70c50101285b8a3ceb0127b87ee32fac7
Dawid Florian <dawidflorian99@gmail.com>
2024-01-06 12:22:22 +0100

    resyme update

--- js/langs.js
+++ js/langs.js
@@ -10,46 +10,61 @@ let langsDict = [
     ["h2_about", "About", "O\u00A0mnie"],
     [
         "p_about", 
-        "Hello! As you probably have already noticed, my name is Dawid Florian, and even though I recently graduated from university, my coding journey began some time ago. I became interested in the .Net platform during my first year of studies, and I have been continuously expanding my skills, gaining new experiences, and improving myself in creating efficient solutions. In my free time, I love going for bike rides.",
-        "Cześć! Jak zapewne już zauważyliście, nazywam się Dawid Florian i choć niedawno ukończyłem studia, moja przygoda z kodowaniem rozpoczęła się już jakiś czas temu. Platformą .Net zainteresowałem się na pierwszym roku studiów, przez cały czas poszerzam swoje umiejętności, zdobywam nowe doświadczenia i doskonalę się w tworzeniu efektywnych rozwiązań. W wolnych chwilach uwielbiam wyskoczyć na rower."
+        "Hello! Welcome to my page. Here, you will find information about me and my experience. I am currently working as a .Net developer. My first encounter with programming was in high school, and I knew then which direction I wanted to develop in. I became interested in the .Net platform in my first year of university, and I have been constantly expanding my skills, gaining new experiences, and perfecting the creation of efficient solutions. In my free time, I love going for bike rides.",
+        "Hej! Miło mi cię gościć na mojej stronie. Znajdziesz tu informacje o mnie oraz o moim doświadczeniu. Obecnie pracuję jako programista .Net. Pierwszą styczność z programowaniem miałem w szkole średniej, już w tedy wiedziałem w jakim kierunku chcę się rozwijać. Platformą .Net zainteresowałem się na pierwszym roku studiów, przez cały czas poszerzam swoje umiejętności, zdobywam nowe doświadczenia i doskonalę się w tworzeniu efektywnych rozwiązań. W wolnych chwilach uwielbiam wyskoczyć na rower."
     ],
     ["ah2_about",".Net Developer", "Programista .Net"],
-    ["p1_about"," Birthday: 6 May 1999", " Urodziny: 6 Maj 1999"],
-    ["p2_about"," Age: 24", " Wiek: 24"],
-    ["p3_about"," Degree: Computer science engineer", " Stopień naukowy: Inżynier IT"],
-    ["p4_about"," Email: dawidflorian99@gmail.com", " Email: dawidflorian99@gmail.com"],
-    ["p5_about"," Website: www.dawidflorian.pl", " Strona internetowa: dawidflorian.pl"],
-    ["p6_about"," City: Krosno / Rzeszów, Poland", " Miasto: Krosno / Rzeszów, Polska"],
+    ["p1_about"," 6 May 1999", " 6 Maj 1999"],
+    ["p2_about"," 24 years old", " 24 lat"],
+    ["p3_about"," Computer science engineer", " Inżynier IT"],
+    ["p4_about"," dawidflorian99@gmail.com", " dawidflorian99@gmail.com"],
+    ["p5_about"," dawidflorian.pl", " dawidflorian.pl"],
+    ["p6_about"," Krosno / Rzeszów, Poland", " Krosno / Rzeszów, Polska"],
 
     ["h2_resume","Resume", "Życiorys"],
     [
         "p_resume",
-        "I graduated this year with a degree in Computer Science and have been immersed in the world of IT for over 8 years since my high school days. Along the way, I've absorbed a wealth of knowledge and skills in the field.", 
-        "Jestem tegorocznym absolwentem studiów informatycznych. Od ponad 8 lat mam do czynienia z informatyką, której uczyłem się od technikum. Przez ten czas przyswoiłem dużo informacji oraz zagadnień związanych z tą dziedziną."
+        "I have been immersed in the world of IT for over 8 years since my high school days. Along the way, I've absorbed a wealth of knowledge and skills in the field.", 
+        "Od ponad 8 lat mam do czynienia z informatyką, której uczyłem się od technikum. Przez ten czas przyswoiłem dużo informacji oraz zagadnień związanych z tą dziedziną."
     ],
+
     ["1h3_resume"," Education", " Edukacja"],
+
     ["1h5_resume","Engineer (Computer since)", "Inżynier (IT)"],
     ["1p_resume","'Rzeszow University of Technology'", "'Politechnika Rzeszowska'"],
     ["2p_resume","From 2019 to 2023", "Od 2019 do 2023"],
     ["3p_resume","Rzeszów, Poland", "Rzeszów, Polska"],
+
     ["2h5_resume","Technician (Computer since)", "Technik (IT)"],
     ["4p_resume","'Complex of Electrical and General Education Schools in Krosno'", "'Zespół Szkół Elektrycznych i Ogólnokształcących w Krośnie'"],
     ["5p_resume","From 2015 to 2019", "od 2015 do 2019"],
     ["6p_resume","Krosno, Poland", "Krosno, Polska"],
+
     ["2h3_resume"," Professional Experience", " Doświadczenie"],
+
     ["3h5_resume","Junior IT Specialist", "Młodszy Specjalista IT"],
-    ["7p_resume","From 2022", "Od 2022"],
+    ["7p_resume","From 2022 (July)", "Od 2022 (Lipiec)"],
     ["8p_resume","Krosno, Poland", "Krosno, Polska"],
     ["1li_resume","Monitoring of services and servers.", "Monitorowanie usług oraz serwerów."],
     ["2li_resume","API testing.", "Testowanie API."],
     ["3li_resume","Manual and regression testing of web and mobile applications.", "Testowanie ręczne, regresyjne aplikacji webowych oraz mobilnych."],
     ["4li_resume","Analysis and resolution of problems reported by customers.", "Analiza oraz rozwiązywanie problemów zgłaszanych przez klientów."],
     ["5li_resume","Documentation management.", "Prowadzenie dokumentacji."],
+
     ["4h5_resume","Academic Internship", "Praktyka zawodowa"],
     ["9p_resume","In 2021", "W 2021"],
     ["10p_resume","Krosno, Poland", "Krosno, Polska"],
     ["6li_resume","Updating content and adding new elements to the company's website.", "Aktualizowanie treści oraz dodawanie nowych elementów na stronę internetową firmy."],
     ["7li_resume","Testing solutions being implemented into the software", "Testowanie rozwiązań wprowadzanych do oprogramowania."],
+
+    ["5h5_resume","Junior .Net Developer", "Młodszy Programista .Net"],
+    ["11p_resume","From 2023 (October)", "Od 2023 (Październik)"],
+    ["12p_resume","Krosno, Poland", "Krosno, Polska"],
+    ["8li_resume","Creating Applications Using the .NET Platform.", "Tworzenie aplikacji z wykorzystaniem platformy .NET."],
+    ["9li_resume","Conducting Unit and Integration Testing.", "Przeprowadzanie testów jednostkowych i integracyjnych."],
+    ["10li_resume","Fixing Bugs and Adding New Features.", "Naprawianie błędów oraz dodawanie nowych funkcjonalności."],
+    ["11li_resume","Creating and Maintaining Detailed Technical and User Documentation.", "Tworzenie i utrzymywanie szczegółowej dokumentacji technicznej i użytkowej."],
+    ["12li_resume","Maintaining Up-to-Date Knowledge of New Technologies and Best Practices in the .NET Industry.", "Utrzymanie aktualnej wiedzy na temat nowych technologii i najlepszych praktyk w branży .NET."],
     
     ["h2_projects","Projects", "Projekty"],
     [
