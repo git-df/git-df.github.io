db072857e6517507eb4d3f7245e973b5a47de674
DAWID <dawidflorian99@gmail.com>
2025-09-23 07:43:25 +0200

    clean

--- new/js/main.js
+++
@@ -1,9 +0,0 @@
-function initializeTyped(id, strings, typeSpeed, backSpeed, cursorChar, loop) {
-    new Typed(`#${id}`, {
-        strings: strings,
-        typeSpeed: typeSpeed,
-        backSpeed: backSpeed,
-        cursorChar: cursorChar,
-        loop: loop
-    });
-}
